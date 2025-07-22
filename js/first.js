889746f6a7915822673cad5ead4be6f7679b60c9
areeba-amirr <areebaamir826@gmail.com>
2025-07-22 20:18:24 +0500

    First Program

---
+++ js/first.js
@@ -0,0 +1,4 @@
+console.log("HELLOO")
+console.log("HELLOO")
+console.log("HELLOO")
+console.log("HELLOO")
---
+++ js/variables.js
@@ -0,0 +1,10 @@
+// console.log("HI")
+// document.getElementById("heading").innerHTML="Hello World";
+let firstName="Areeba";
+var lastName="Amir";
+console.log(firstName);
+console.log(lastName);
+Fullname="Areeba Aamir";
+// console.log("Fullname");
+console.log(Fullname);
+// document.getElementById("heading").innerHTML="AREEBA AMIR";
