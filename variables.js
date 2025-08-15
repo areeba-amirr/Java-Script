15e814cd974b4fd27f9d9de366c9a877742bfad6
areeba-amirr <areebaamir826@gmail.com>
2025-08-15 16:33:28 +0500

    Async + Defer and variables

---
+++ variables.js
@@ -0,0 +1,19 @@
+console.log("HI");
+// document.getElementById("heading").innerHTML="Hello World";//on browser
+//declaration
+let firstName;
+//initialization
+firstName = "Areeba";
+//declaration + initialization
+var lastName = "Amir";
+console.log(firstName); //on console
+console.log(lastName);
+Fullname = "Areeba Aamir";
+console.log("Fullname");
+console.log(Fullname);
+// document.getElementById("heading").innerHTML="AREEBA AMIR"; //on brower
+const age = 18;
+firstName = "Welcome"; //can be changed
+// age = 20; //cant be changed ERROR
+console.log(age);
+console.log(firstName);
