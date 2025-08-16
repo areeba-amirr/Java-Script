317c9263bb3efb001dc5d6ea4dd5a9caba5601f3
areeba-amirr <areebaamir826@gmail.com>
2025-08-16 13:38:00 +0500

    Switch Statement and Practice Question

---
+++ switch.js
@@ -0,0 +1,23 @@
+let num1 = 4
+let num2 = 9
+let choice = 4
+switch(choice){
+     case 1 : console.log("Sum=",num1+num2);  break;
+     case 2 : console.log("Diff=",num1-num2); break;
+     case 3 : console.log("Multiply=",num1*num2); break;
+     case 4 : console.log("Div=",num1/num2); break;
+     case 5 : console.log("Mod=",num1%num2); break;
+     default : console.log("Inalid choice"); break;
+}
+let day = 4
+switch(day){
+  case 1 : console.log("Monday");break;
+  case 2 : console.log("Tuesday");break;
+  case 3 : console.log("Wednesday");break;
+  case 4 : console.log("Thursday");break;
+  case 5 : console.log("Friday");break;
+  case 6 : console.log("Saturday");break;
+  case 7 : console.log("Sunday");break;
+  default : console.log("Invalid Choice");
+}
+
