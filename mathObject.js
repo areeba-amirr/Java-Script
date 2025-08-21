857005b7c2adc816879fd45fce493839793d67a4
areeba-amirr <areebaamir826@gmail.com>
2025-08-21 15:56:50 +0500

    Math Objects

---
+++ mathObject.js
@@ -0,0 +1,33 @@
+//1)Absolute Value
+let value = -3
+console.log(Math.abs(value))
+//2)Round off
+let no = 3.67
+console.log(Math.round(no))
+//3)Constant Value
+console.log(Math.PI)
+//4) Ceil (round up)
+let no1 = 2.4
+console.log(Math.ceil(no1))
+//5) Floe (round down)
+console.log(Math.floor(no1))
+//6)Truncate(ignore valuesafter point)
+console.log(Math.trunc(no1))
+//7) Power
+console.log(Math.pow(2,3)) //base,power
+//8) Square Root
+console.log(Math.sqrt(16))
+//9)Minimum Value
+console.log(Math.min(3,5,2,7,5,8,9,1))
+//10)Maximum Value
+console.log(Math.max(2,4,6,9,0))
+//11)Return Sign(return -1 if negative,1 if positive and 0 if zero)
+console.log(Math.sign(-3))
+//12)Logarithm
+console.log(Math.log(22))
+
+
+
+
+
+
