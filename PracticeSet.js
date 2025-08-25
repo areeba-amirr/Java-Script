cd1cc25d3c5ae94c4b46dd763ff37ceda7a77c39
areeba-amirr <areebaamir826@gmail.com>
2025-08-25 21:23:13 +0500

    Practice Questions

---
+++ PracticeSet.js
@@ -0,0 +1,30 @@
+//Question 1
+let str = "Areeba"
+let no = 9
+console.log(str +" "+ no)
+//Question 2
+console.log(typeof str)
+//Question 3
+const student = {
+  name : "Areeba",
+  class : "1st year"
+}
+console.log(student)
+// student = 88  //error
+//Question 4
+student.age = 19;
+console.log(student)
+//Question 5
+const dictionary = {
+  Serendipity : "Luck",
+  Tenacity : "Persistence",
+  Ephemeral : "Temporary",
+  Meticulous : "Precise",
+  Resilient : "Strong"
+}
+console.log("*************DICTIONARY************")
+console.log("Serendipity : "+dictionary.Serendipity)
+console.log("Tenacity : "+dictionary.Tenacity)
+console.log("Ephemeral : "+dictionary.Ephemeral)
+console.log("Meticulous : "+dictionary.Meticulous)
+console.log("Resilient : "+dictionary.Resilient)
