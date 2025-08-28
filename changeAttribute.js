d72aec94a02ac81dcef387c76f9544993a446425
areeba-amirr <areebaamir826@gmail.com>
2025-08-28 18:44:55 +0500

    DOM Manipulation get+set+has Attribute

---
+++ changeAttribute.js
@@ -0,0 +1,19 @@
+let heading = document.getElementById("heading");
+//set attribute
+heading.setAttribute("class","heading-class");
+//get attribute
+console.log(heading.getAttribute("class"))
+//remove attribute
+heading.removeAttribute("class")
+
+//Check whether attribute is prsent or not
+let result = heading.hasAttribute("class")
+console.log(result) //true
+ result =heading.hasAttribute("name")
+ console.log(result)
+
+ let unOrdered = document.getElementById("my-ul");
+ unOrdered.setAttribute("class","my class");
+ console.log(unOrdered.hasAttribute("id"))
+ unOrdered.removeAttribute("id")
+console.log(unOrdered.getAttribute("class"))
