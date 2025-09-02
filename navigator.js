2452b8ea85e1ec025bc1957e1348f950b5aa872b
areeba-amirr <areebaamir826@gmail.com>
2025-09-02 13:25:57 +0500

    BOM Navigator Object

---
+++ navigator.js
@@ -0,0 +1,8 @@
+console.log(navigator.appName)
+console.log(navigator.appVersion)
+console.log(navigator.appCodeName)
+console.log(navigator.cookieEnabled) //returns boolean value
+console.log(navigator.userAgent)
+console.log(navigator.platform)
+console.log(navigator.javaEnabled()) //return boolean value
+
