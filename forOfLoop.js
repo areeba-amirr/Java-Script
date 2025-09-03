6cb93ec7bec4d9f84064ca1de29eafe226479e38
areeba-amirr <areebaamir826@gmail.com>
2025-09-03 17:52:30 +0500

    ES6 For Of Loop

---
+++ forOfLoop.js
@@ -0,0 +1,15 @@
+let arr = [1,2,3,4,5,6,7,8];
+for(let value of arr){
+  console.log(value)
+}
+
+let str = "Areeba Muhammad Aamir"
+for(let variable of str){
+  console.log(variable)
+}
+let sum = 0;
+for(let value of arr){
+  sum += value;
+  console.log(value)
+}
+console.log(sum)
