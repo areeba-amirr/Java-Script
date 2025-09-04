9ec31cb9f1abd01b3e32757d9f832330cf64d502
areeba-amirr <areebaamir826@gmail.com>
2025-09-04 15:19:45 +0500

    ES6 Template Literals

---
+++ templateLiterals.js
@@ -0,0 +1,21 @@
+let name = "Areeba \n Aamir"
+console.log(name)
+//template Literals
+let ele = `Areeba
+
+
+
+
+
+
+Aamir`;
+console.log(ele)
+ele = `are   ba    amir`;
+console.log(ele);
+//conacatenation with template literals
+let me = "Areeba Aamir";
+let age = 19;
+console.log(`Hello ,I am ${me} and I am ${age}`)
+let no = 8;
+console.log(`8 x 6 = ${no*6}`)
+console.log(`8 x 9 = ${no*9}`)
