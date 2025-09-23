fb1423656a65996a66512068ee38325f55fc03a6
areeba-amirr <areebaamir826@gmail.com>
2025-09-23 19:01:58 +0500

    Error Handling

---
+++ errors.js
@@ -0,0 +1,35 @@
+setTimeout(() => {
+  console.log("Hacking Wifi.....");
+}, 1000);
+try{
+console.log(hello)
+}
+catch(error){
+  console.log("Some error occur") //disply error on vconsole so program soednot stop
+  console.log(error)
+}
+setTimeout(() => {
+  console.log("Fetching username and pssword");
+}, 2000);
+setTimeout(() => {
+  console.log("Fetching in process.........");
+}, 3000);
+setTimeout(() => {
+  console.log("Fetched..!")
+}, 4000);
+
+setTimeout(() => {
+  console.log("Hacking.....");
+  try{
+    console.log(hack)
+  }
+  catch(err){
+    console.log("Something wrong")
+  }
+}, 1000);
+setTimeout(() => {
+  console.log("Hacking Wifi.....");
+}, 1000);
+setTimeout(() => {
+  console.log("Hacking Wifi.....");
+}, 1000);
